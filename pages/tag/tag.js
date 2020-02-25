00a37145fadbc307f4817f13323d150c34984e1f
yumogu <1123671761@qq.com>
2020-02-25 23:27:33 +0800

    20200225开发标签页

--- pages/tag/tag.js
+++ pages/tag/tag.js
@@ -6,9 +6,138 @@ Page({
   data: {
     homeFlag: false,
     tagFlag: false,
-    myFlag: true
+    myFlag: true,
+    bookList: [
+      {
+        bookName: '追风筝的人',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: 'c测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      },
+      {
+        bookName: '侧测试',
+        bookImgUrl:
+          'https://ae01.alicdn.com/kf/He8394015d77944f19d085f9062711172U.png'
+      }
+    ],
+    typeList: [{
+      index: 1,
+      type: '测试1',
+      selected: false
+    }, {
+      index: 2,
+      type: '测试2',
+      selected: false
+    }, {
+      index: 3,
+      type: '测试3',
+      selected: false
+    }]
   },
   onLoad: function(options) {
     wx.hideTabBar()
+  },
+  checkboxChange (e) {
+    console.log('checkboxChange e:',e);
+    let string = "typeList["+e.target.dataset.index+"].selected"
+    console.log('e.target.dataset.index', e.target.dataset.index)
+    console.log('this.data.typeList', this.data.typeList)
+
+    console.log('this.data.typeList[e.target.dataset.index].selected', this.data.typeList[e.target.dataset.index].selected)
+    this.setData({
+        [string]: !this.data.typeList[e.target.dataset.index].selected
+    })
+    let detailValue = this.data.typeList.filter(it => it.selected).map(it => it.type)
+    console.log('所有选中的值为：', detailValue)
   }
 })
+// //index.js
+// //获取应用实例
+// const app = getApp()
+
+// Page({
+//   data: {
+//     userInfo: {},
+    
+//   },
+//   onLoad: function(options) {
+//     wx.setNavigationBarTitle({
+//       title: '阅读记录'
+//     })
+//     this.setData({
+//       userInfo: app.globalData.userInfo
+//     })
+//   },
+//   back: function() {
+//     wx.navigateBack()
+//   }
+// })
